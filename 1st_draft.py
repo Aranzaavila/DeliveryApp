3e1ff83b76b6ccf976203aa1bdc700cc0d17b5a7
aranza.avila <ariavirod@gmail.com>
2025-04-23 13:04:35 +0200

    classes

---
+++ 1st_draft.py
@@ -0,0 +1,29 @@
+
+
+class Company:
+    def__init__(self,name):
+    self.name=name
+    self.clientes=[]
+
+class Client:
+    def__init__(self, name, email, company):
+    self.name=name
+    self.email=email
+    self.company=company
+
+
+class Freelancer:
+    def__init__(self, name, email, work_hours ):
+    self.name=name
+    self.email=email
+    self.work_hours=work_hours
+    self.project=[]
+
+
+class Project:
+    def__init__(self, title,)
+
+class Invoice:
+    def__init__(self,)
+
+
